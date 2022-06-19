2d710486231f6b63b68099c16ad2d1574d236a78
Alexander Suvorov <44779234+whataperson@users.noreply.github.com>
2022-06-19 11:17:52 +0300

    added solution for 2 task

--- develop/dev02/task.go
+++ develop/dev02/task.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+	"unicode"
+	"unicode/utf8"
+)
+
 /*
 === Задача на распаковку ===
 
@@ -18,6 +27,39 @@ package main
 Функция должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+func UnpackString(str string) (string, error) {
+	var result strings.Builder
+	var stack rune
+	var isLetter bool
+	for i, r := range str {
+		if unicode.IsDigit(r) && i == 0 {
+			return "", fmt.Errorf("error in string: %s", str)
+		}
+		if unicode.IsLetter(r) {
+			if isLetter {
+				result.WriteRune(stack)
+			}
+			if i == utf8.RuneCountInString(str)-1 {
+				result.WriteRune(r)
+			}
+			stack = r
+			isLetter = true
+		}
+		if unicode.IsDigit(r) {
+			count, _ := strconv.Atoi(string(r))
+			for j := 0; j < count; j++ {
+				result.WriteRune(stack)
+			}
+			isLetter = false
+		}
+	}
+	return result.String(), nil
+}
 
+func main() {
+	unpackStr, err := UnpackString("a4bc2d5e")
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(unpackStr)
 }
--- develop/dev02/task_test.go
+++ develop/dev02/task_test.go
@@ -1,1 +1,39 @@
 package main
+
+import "testing"
+
+type Case struct {
+	value string
+	equal string
+}
+
+func TestUnpackString(t *testing.T) {
+	testCases := []Case{
+		{
+			value: "45",
+			equal: "",
+		},
+		{
+			value: "a4bc2d5e",
+			equal: "aaaabccddddde",
+		},
+		{
+			value: "abcd",
+			equal: "abcd",
+		},
+		{
+			value: "",
+			equal: "",
+		},
+		{
+			value: "abcd5tyuioop",
+			equal: "abcdddddtyuioop",
+		},
+	}
+	for _, v := range testCases {
+		result, _ := UnpackString(v.value)
+		if result != v.equal {
+			t.Errorf("For: %s. Expected: %s. Got: %s.", v.value, v.equal, result)
+		}
+	}
+}
