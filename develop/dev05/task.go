c018781a45fab14357426b4fe9c14e62ef8da33e
Alexander Suvorov <44779234+whataperson@users.noreply.github.com>
2022-06-23 22:58:34 +0300

    added functions to 5 task

--- develop/dev05/task.go
+++ develop/dev05/task.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
 /*
 === Утилита grep ===
 
@@ -17,7 +26,61 @@ package main
 
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
+func readFile(filename string) []string {
+	var result []string
+	f, err := os.Open(filename)
 
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		result = append(result, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return result
+}
+
+//Пример запуска программы: go run task.go -A=1 "in.txt" "1"
 func main() {
+	a := flag.Int("A", 0, "after")
+	b := flag.Int("B", 0, "before")
+	c := flag.Int("C", 0, "context")
+	flag.Parse()
+	args := flag.Args()
+	filename := args[0]
+	pattern := args[1]
+	stringsFromFile := readFile(filename)
+	//result := make([]string, len(stringsFromFile))
+	bottom := 0
+	top := 0
+	switch {
+	case *a != 0:
+		bottom = *a
+	case *b != 0:
+		top = *b
+	case *c != 0:
+		top, bottom = *c, *c
+	}
+	for index, value := range stringsFromFile {
+		if strings.Contains(value, pattern) {
+			topIndex := index - top
+			bottomIndex := index + bottom
+			if bottomIndex < len(stringsFromFile) || topIndex < 0 {
+				for i := topIndex; i <= bottomIndex; i++ {
+					fmt.Println(stringsFromFile[i])
+				}
+			} else {
+				fmt.Println(stringsFromFile[index])
+			}
+		}
+	}
 
 }
