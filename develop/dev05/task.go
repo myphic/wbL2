21d2a947ee8d1a9f0fc9712694bdb4389814df82
Alexander Suvorov <44779234+whataperson@users.noreply.github.com>
2022-06-24 20:41:46 +0300

    added flag handlers to 5 task

--- develop/dev05/task.go
+++ develop/dev05/task.go
@@ -48,11 +48,24 @@ func readFile(filename string) []string {
 	return result
 }
 
+func printLine(lineNum bool, value string, index int) {
+	if lineNum {
+		fmt.Println("number: ", index+1, "value: ", value)
+	} else {
+		fmt.Println(value)
+	}
+}
+
 //Пример запуска программы: go run task.go -A=1 "in.txt" "1"
 func main() {
-	a := flag.Int("A", 0, "after")
-	b := flag.Int("B", 0, "before")
-	c := flag.Int("C", 0, "context")
+	A := flag.Int("A", 0, "after")
+	B := flag.Int("B", 0, "before")
+	C := flag.Int("C", 0, "context")
+	c := flag.Bool("c", false, "count")
+	i := flag.Bool("i", false, "ignore-case")
+	v := flag.Bool("v", false, "invert")
+	F := flag.Bool("F", false, "fixed")
+	n := flag.Bool("n", false, "line num")
 	flag.Parse()
 	args := flag.Args()
 	filename := args[0]
@@ -61,26 +74,47 @@ func main() {
 	//result := make([]string, len(stringsFromFile))
 	bottom := 0
 	top := 0
+
 	switch {
-	case *a != 0:
-		bottom = *a
-	case *b != 0:
-		top = *b
-	case *c != 0:
-		top, bottom = *c, *c
+	case *A != 0:
+		bottom = *B
+	case *B != 0:
+		top = *B
+	case *C != 0:
+		top, bottom = *C, *C
 	}
+	counter := 0
 	for index, value := range stringsFromFile {
-		if strings.Contains(value, pattern) {
-			topIndex := index - top
-			bottomIndex := index + bottom
-			if bottomIndex < len(stringsFromFile) || topIndex < 0 {
-				for i := topIndex; i <= bottomIndex; i++ {
-					fmt.Println(stringsFromFile[i])
+		if *F && value == pattern {
+			printLine(*n, value, index)
+		}
+		if *i {
+			value = strings.ToLower(value)
+			pattern = strings.ToLower(pattern)
+		}
+		if *v {
+			if !strings.Contains(value, pattern) {
+				printLine(*n, value, index)
+			}
+		}
+		if !*v && !*F {
+			if strings.Contains(value, pattern) {
+				counter++
+				if !*c {
+					topIndex := index - top
+					bottomIndex := index + bottom
+					if bottomIndex < len(stringsFromFile) || topIndex < 0 {
+						for i := topIndex; i <= bottomIndex; i++ {
+							printLine(*n, stringsFromFile[i], index)
+						}
+					} else {
+						printLine(*n, stringsFromFile[index], index)
+					}
 				}
-			} else {
-				fmt.Println(stringsFromFile[index])
 			}
 		}
 	}
-
+	if *c {
+		fmt.Println(counter)
+	}
 }
