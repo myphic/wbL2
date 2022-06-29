54af32ef1f9dcc1080943cf08eb70351c1cd2a2c
Alexander Suvorov <44779234+whataperson@users.noreply.github.com>
2022-06-29 23:12:23 +0300

    added getting images from site

--- develop/dev09/task.go
+++ develop/dev09/task.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"golang.org/x/net/html"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 /*
@@ -21,14 +23,40 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error with getting site")
 	}
-	writeFile("result.html", resp)
+	s := writeFile("result.html", resp)
+	doc, err := html.Parse(strings.NewReader(s))
+	if err != nil {
+		// ...
+	}
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "img" {
+			for _, val := range n.Attr {
+				if val.Key == "src" || val.Key == "data-src" {
+					resp, err := http.Get(os.Args[1] + val.Val)
+					splitUrl := strings.Split(val.Val, "/")
+
+					defer resp.Body.Close()
+					if err != nil {
+						log.Fatalf("Error with getting site")
+					}
+
+					writeFile("assets/images/"+splitUrl[len(splitUrl)-1], resp)
+				}
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+	f(doc)
 }
 
-func writeFile(filename string, m *http.Response) {
+func writeFile(filename string, m *http.Response) string {
 	result, err := os.Create(filename)
 	defer result.Close()
 	if err != nil {
-		log.Print("Error with create file")
+		log.Print("Error with create file", err)
 	}
 
 	body, err := ioutil.ReadAll(m.Body)
@@ -36,4 +64,6 @@ func writeFile(filename string, m *http.Response) {
 		log.Println("Error with reading body")
 	}
 	result.WriteString(string(body))
+	log.Println("File created:", filename)
+	return string(body)
 }
