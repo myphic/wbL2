caee9f643cbdcace9dbc723445e35e122bc7695f
Alexander Suvorov <44779234+whataperson@users.noreply.github.com>
2022-07-01 22:55:06 +0300

    wrote a function for console commands

--- develop/dev08/task.go
+++ develop/dev08/task.go
@@ -1,19 +1,54 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
 /*
-=== Взаимодействие с ОС ===
+Необходимо реализовать свой собственный UNIX-шелл-утилиту с поддержкой ряда простейших команд:
 
-Необходимо реализовать собственный шелл
+- cd <args> - смена директории (в качестве аргумента могут быть то-то и то)
+- pwd - показать путь до текущего каталога
+- echo <args> - вывод аргумента в STDOUT
+- kill <args> - "убить" процесс, переданный в качесте аргумента (пример: такой-то пример)
+- ps - выводит общую информацию по запущенным процессам в формате *такой-то формат*
 
-встроенные команды: cd/pwd/echo/kill/ps
-поддержать fork/exec команды
-конвеер на пайпах
+Так же требуется поддерживать функционал fork/exec-команд
 
-Реализовать утилиту netcat (nc) клиент
-принимать данные из stdin и отправлять в соединение (tcp/udp)
-Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
+Дополнительно необходимо поддерживать конвейер на пайпах (linux pipes, пример cmd1 | cmd2 | .... | cmdN).
 */
 
 func main() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("> ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		if err = execInput(input); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
+}
 
+func execInput(input string) error {
+	input = strings.TrimSuffix(input, "\n")
+
+	args := strings.Split(input, " ")
+
+	switch args[0] {
+	case "cd":
+		return os.Chdir(args[1])
+	case "exit":
+		os.Exit(0)
+	}
+	cmd := exec.Command(args[0], args[1:]...)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	return cmd.Run()
 }
